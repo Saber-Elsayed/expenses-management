53643f579c853f41299c0c3ce31bb69e3a9d8c25
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-03 17:03:18 +0200

    setup server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const expensesRoute = require("../Expensess Management/servers/routes/api");
 const bodyParser = require("body-parser");
 const path = require("path");
 const api = require("./servers/routes/api");
@@ -6,7 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", api);
+mongoose.connect("mongodb://127.0.0.1:27017/Expenses", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use("/expenses", expensesRoute);
 
 const port = 3000;
 app.listen(port, function () {
