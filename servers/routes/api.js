6beec0b1e6ca4fd7210423cf0b62ccf28af59c43
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-04 11:32:03 +0200

    post query

--- servers/routes/api.js
+++ servers/routes/api.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const moment = require("moment");
+
+const bodyParser = require("body-parser");
 const router = express.Router();
 const Expense = require("../../model/expensess");
 router.get("/", async (req, res) => {
+  console.log("fdfdf");
+
   try {
     const expenses = await Expense.find().sort({ date: -1 });
     res.json(expenses);
@@ -11,4 +16,42 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const { item, amount, group, date } = req.body;
+    console.log(req.body);
+
+    const validGroups = ["fun", "food", "rent", "bills", "misc"];
+    if (!validGroups.includes(group)) {
+      return res.status(400).json({ error: "Invalid expense group" });
+    }
+
+    const expenseDate = date
+      ? moment(date).format("LLLL")
+      : moment().format("LLLL");
+
+    const newExpense = new Expense({
+      item,
+      amount,
+      group,
+      date: expenseDate,
+    });
+
+    newExpense
+      .save()
+      .then((savedExpense) => {
+        console.log(
+          `Expense of $${savedExpense.amount} spent on ${savedExpense.item} saved to the database.`
+        );
+        res.status(201).json(savedExpense);
+      })
+      .catch((saveError) => {
+        console.error("Error saving expense to the database:", saveError);
+        res.status(500).json({ error: "Internal Server Error" });
+      });
+  } catch (error) {
+    console.error("Error creating expense:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 module.exports = router;
