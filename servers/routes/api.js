1a83e05d3cce3bddb01ecb82134756e457512621
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-01-04 10:49:21 +0200

    sort date query

--- servers/routes/api.js
+++ servers/routes/api.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const Expense = require("../../model/expensess");
 router.get("/", async (req, res) => {
   try {
-    const expenses = await Expense.find();
+    const expenses = await Expense.find().sort({ date: -1 });
     res.json(expenses);
   } catch (error) {
     console.error("Error fetching expenses:", error);
